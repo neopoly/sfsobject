90e6a87a23fb2b417a5febf654a7a061eb71fd23
Peter Suschlik <peter@suschlik.de>
2014-07-09 00:36:43 +0200

    First baby steps

--- lib/sfsobject/map.ex
+++ lib/sfsobject/map.ex
@@ -1,2 +1,11 @@
 defmodule SFSObject.Map do
+  defstruct data: %{}
+
+  def new, do: %__MODULE__{}
+
+  def size(map) do
+    Map.size(map.data)
+  end
+
+  def empty?(map), do: __MODULE__.size(map) == 0
 end
--- test/sfsobject/map_test.exs
+++ test/sfsobject/map_test.exs
@@ -1,7 +1,13 @@
 defmodule SFSObject.MapTest do
   use ExUnit.Case
 
-  test "loads" do
-    assert true
+  test "empty map" do
+    map = new_map
+    assert SFSObject.Map.size(map) == 0
+    assert SFSObject.Map.empty?(map)
+  end
+
+  defp new_map do 
+    SFSObject.Map.new
   end
 end
