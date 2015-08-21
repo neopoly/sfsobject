9036ee7c87129c6b60d8ea56cdbc11420beaefc0
Peter Suschlik <ps@neopoly.de>
2015-08-21 16:36:55 +0200

    Implement most simple data wrappers

--- lib/sfsobject.ex
+++ lib/sfsobject.ex
@@ -1,2 +1,112 @@
 defmodule SFSObject do
+  defstruct data: %{}
+
+  defmodule DataWrapper do
+    defstruct [:type, :value]
+
+    def new(type, value) do
+      %DataWrapper{type: type, value: value}
+    end
+  end
+
+  def new() do
+    %SFSObject{}
+  end
+
+  def put_null(object, key) do
+    put_data(object, key, :null, :null)
+  end
+
+  def is_null?(object, key) do
+    get_data(object, key, :null) == :null
+  end
+
+  def put_bool(object, key, value) when is_boolean(value) do
+    put_data(object, key, :bool, value)
+  end
+
+  def get_bool(object, key) do
+    get_data(object, key, :bool)
+  end
+
+  def put_byte(object, key, value) when is_integer(value) do
+    put_data(object, key, :byte, value)
+  end
+
+  def get_byte(object, key) do
+    get_data(object, key, :byte)
+  end
+
+  def put_short(object, key, value) when is_integer(value) do
+    put_data(object, key, :short, value)
+  end
+
+  def get_short(object, key) do
+    get_data(object, key, :short)
+  end
+
+  def put_int(object, key, value) when is_integer(value) do
+    put_data(object, key, :int, value)
+  end
+
+  def get_int(object, key) do
+    get_data(object, key, :int)
+  end
+
+  def put_long(object, key, value) when is_number(value) do
+    put_data(object, key, :long, value)
+  end
+
+  def get_long(object, key) do
+    get_data(object, key, :long)
+  end
+
+  def put_float(object, key, value) when is_float(value) do
+    put_data(object, key, :float, value)
+  end
+
+  def get_float(object, key) do
+    get_data(object, key, :float)
+  end
+
+  def put_double(object, key, value) when is_float(value) do
+    put_data(object, key, :double, value)
+  end
+
+  def get_double(object, key) do
+    get_data(object, key, :double)
+  end
+
+  def put_string(object, key, value) when is_binary(value) do
+    put_data(object, key, :string, value)
+  end
+
+  def get_string(object, key) do
+    get_data(object, key, :string)
+  end
+
+  # BOOL_ARRAY(9),
+  # BYTE_ARRAY(10),
+  # SHORT_ARRAY(11),
+  # INT_ARRAY(12),
+  # LONG_ARRAY(13),
+  # FLOAT_ARRAY(14),
+  # DOUBLE_ARRAY(15),
+  # UTF_STRING_ARRAY(16),
+  # SFS_ARRAY(17),
+  # SFS_OBJECT(18),
+  # CLASS(19);
+
+  defp put_data(%SFSObject{data: data} = object, key, type, value) do
+    wrapped = SFSObject.DataWrapper.new(type, value)
+    data = Map.put(data, key, wrapped)
+    %{object | data: data}
+  end
+
+  defp get_data(%SFSObject{data: data}, key, type) do
+    case Map.fetch(data, key) do
+      {:ok, %SFSObject.DataWrapper{type: ^type, value: value}} -> value
+      _ -> nil
+    end
+  end
 end
--- test/sfsobject_test.exs
+++ test/sfsobject_test.exs
@@ -1,7 +1,104 @@
 defmodule SFSObjectTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "new" do
+    object = SFSObject.new
+    assert object.data == %{}
+  end
+
+  test "null" do
+    object = SFSObject.new
+    assert SFSObject.is_null?(object, "key") == false
+
+    object = SFSObject.put_null(object, "key")
+    assert SFSObject.is_null?(object, "key") == true
+  end
+
+  test "bool" do
+    object = SFSObject.new
+    assert SFSObject.get_bool(object, "key") == nil
+
+    object = SFSObject.put_bool(object, "key", true)
+    assert SFSObject.get_bool(object, "key") == true
+
+    object = SFSObject.put_bool(object, "key", false)
+    assert SFSObject.get_bool(object, "key") == false
+  end
+
+  test "byte" do
+    object = SFSObject.new
+    assert SFSObject.get_byte(object, "key") == nil
+
+    object = SFSObject.put_byte(object, "key", 1)
+    assert SFSObject.get_byte(object, "key") == 1
+
+    object = SFSObject.put_byte(object, "key", -2)
+    assert SFSObject.get_byte(object, "key") == -2
+  end
+
+  test "short" do
+    object = SFSObject.new
+    assert SFSObject.get_short(object, "key") == nil
+
+    object = SFSObject.put_short(object, "key", 1)
+    assert SFSObject.get_short(object, "key") == 1
+
+    object = SFSObject.put_short(object, "key", -2)
+    assert SFSObject.get_short(object, "key") == -2
+  end
+
+  test "int" do
+    object = SFSObject.new
+    assert SFSObject.get_int(object, "key") == nil
+
+    object = SFSObject.put_int(object, "key", 1)
+    assert SFSObject.get_int(object, "key") == 1
+
+    object = SFSObject.put_int(object, "key", -2)
+    assert SFSObject.get_int(object, "key") == -2
+  end
+
+  test "long" do
+    object = SFSObject.new
+    assert SFSObject.get_long(object, "key") == nil
+
+    object = SFSObject.put_long(object, "key", 1)
+    assert SFSObject.get_long(object, "key") == 1
+
+    object = SFSObject.put_long(object, "key", -2)
+    assert SFSObject.get_long(object, "key") == -2
+  end
+
+  test "float" do
+    object = SFSObject.new
+    assert SFSObject.get_float(object, "key") == nil
+
+    object = SFSObject.put_float(object, "key", 1.0)
+    assert SFSObject.get_float(object, "key") == 1.0
+
+    object = SFSObject.put_float(object, "key", -2.0)
+    assert SFSObject.get_float(object, "key") == -2.0
+  end
+
+  test "double" do
+    object = SFSObject.new
+    assert SFSObject.get_double(object, "key") == nil
+
+    object = SFSObject.put_double(object, "key", 1.0)
+    assert SFSObject.get_double(object, "key") == 1.0
+
+    object = SFSObject.put_double(object, "key", -2.0)
+    assert SFSObject.get_double(object, "key") == -2.0
+  end
+
+  test "string" do
+    object = SFSObject.new
+    assert SFSObject.get_string(object, "key") == nil
+
+    object = SFSObject.put_string(object, "key", "José")
+    assert SFSObject.get_string(object, "key") == "José"
+
+    object = SFSObject.put_string(object, "key", "other")
+    assert SFSObject.get_string(object, "key") == "other"
   end
 end
