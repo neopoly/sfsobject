f1f4d31437bc862f238458325a1a996468b2cbd1
Peter Suschlik <ps@neopoly.de>
2015-10-26 15:30:12 +0100

    Release v0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SFSObject.Mixfile do
 
   def project do
     [app: :sfsobject,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      name: "sfsobject",
      description: "Encode/decode SFSObjects",
